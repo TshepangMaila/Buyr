9d397160dda6a6d7e5f9a5445d5ef1e465373d6a
TshepangMaila <tshepang.maila@sambeconsulting.com>
2021-08-10 14:54:44 +0200

    started the configuration process

--- api/app.py
+++ api/app.py
@@ -1,3 +1,17 @@
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from routes import routes
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = 'buyrrr'
+
+api = Api(app)
+
+routes(api)
+
+if __name__ == '__main__':
+	app.run(port=5000)
---
+++ api/routes.py
@@ -0,0 +1,4 @@
+
+
+def routes(api):
+    pass
